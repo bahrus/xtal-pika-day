8b7048b356605bec8843a3812699f4e21004506f
bahrus <anderson.bruce.b@gmail.com>
2018-05-30 21:37:50 -0400

    ...

--- xtal-pika-day.ts
+++ xtal-pika-day.ts
@@ -1,11 +1,15 @@
-;
+declare var xtal_pika_day: HTMLLinkElement;
+declare class Pikaday {
+    constructor(obj);
+ };
+
 (function () {
-    const cs_src = self['xtal_pika_day'] ? xtal_pika_day.href : document.currentScript.src;
+    const cs_src = self['xtal_pika_day'] ? xtal_pika_day.href : (document.currentScript as HTMLScriptElement).src;
     const base = cs_src.split('/').slice(0, -1).join('/');
     if (typeof Pikaday === 'undefined') {
-        const link = document.createElement('link');
+        const link = document.createElement('link') as HTMLLinkElement;
         link.rel = 'stylesheet';
-        link.href = base + '/pikaday.css';
+        link.href = base + '/pikaday.css'
         document.head.appendChild(link);
     }
     //const template = document.createElement('template');
@@ -18,53 +22,60 @@
     //      :host {
     //         display: block;
     //     }
+        
     // </style>
     // <input type="text" id="datepicker">
     // `;
-    if (typeof Pikaday === 'undefined') {
-        downloadPikaJs();
-    }
-    else {
-        initXtalPikaDay();
-    }
-    //})
+            if (typeof Pikaday === 'undefined') {
+                downloadPikaJs();
+            }else{
+                initXtalPikaDay();
+            }
+
+        //})
     //})
     function downloadPikaJs() {
         const pathToPik = base + '/pikaday.js';
         const script = document.createElement('script');
         script.src = pathToPik;
+
         script.addEventListener('load', e => {
-            initXtalPikaDay();
+            initXtalPikaDay()
         });
         document.head.appendChild(script);
     }
-    function initXtalPikaDay() {
-        class XtalPikaDay extends HTMLElement {
-            static get is() { return 'xtal-pika-day'; }
+    function initXtalPikaDay(){
+        class XtalPikaDay extends HTMLElement{
+            static get is(){return 'xtal-pika-day';}
             constructor() {
                 super();
                 //this.attachShadow({ mode: 'open' });
-                // this.addTemplate();
+               // this.addTemplate();
+                
             }
-            connectedCallback() {
-                this.innerHTML = `<input type="text" id="datepicker">`;
-                this.config = {};
+            connectedCallback(){
+                this.innerHTML =  `<input type="text" id="datepicker">`;
+                this.config={};
             }
-            get config() {
+            // addTemplate(){
+            //     //this.shadowRoot.appendChild(template.content.cloneNode(true));
+            //     //this.config = {};
+            // }
+            _config : object;
+            get config(){
                 return this._config;
             }
-            set config(val) {
+            set config(val){
                 this._config = val;
                 this._config['field'] = this.querySelector('#datepicker');
                 this.onPropsChange();
             }
-            onPropsChange() {
+            onPropsChange(){
                 const picker = new Pikaday(this._config);
             }
         }
-        if (customElements.get(XtalPikaDay.is))
-            return;
+        if(customElements.get(XtalPikaDay.is)) return;
         customElements.define(XtalPikaDay.is, XtalPikaDay);
     }
+
 })();
-//# sourceMappingURL=xtal-pika-day.js.map
